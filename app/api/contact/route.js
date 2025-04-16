244673871e94a15e9a26f2c1c4d75d2bb0488ba3
shivam-holboxai <shivam@holbox.ai>
2025-04-16 15:03:35 +0530

    set up

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -10,8 +10,11 @@ export async function POST(req) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.SMTP_USER,
-        pass:  process.env.SMTP_PASS,
+        // user: process.env.SMTP_USER,
+        // pass:  process.env.SMTP_PASS,
+        user: "shivam@holbox.ai",
+        pass:  "xqvn luew sysm vbyt",
+
       },
     });
 
